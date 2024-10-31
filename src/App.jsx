01d24346546ffb4c86b7ebbf81bbb295d58b0912
Natalia Kur <natalia.kur@frontkom.com>
2024-10-31 14:37:01 +0100

    Add axios and api key, rename files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.scss";
+import Dictionary from "./components/Dictionary.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">Dictionary project</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>Coded by </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Dictionary.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Dictionary() {
+  let [keyword, setKeyWord] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  function search(evt) {
+    evt.preventDefault();
+    alert(`Searching for ${keyword}`);
+    const apiKey = process.env.REACT_APP_DICTIONARY_API_KEY;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(evt) {
+    setKeyWord(evt.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
